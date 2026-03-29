1068803f5f07e1857d52f14f41867e4365e02b80
aiworkspacemohit <ai.workspace.mohit@gmail.com>
2026-03-29 21:10:04 +0530

    end of practice_day_2

--- patterns/hollow_pattern.java
+++ patterns/hollow_pattern.java
@@ -2,10 +2,10 @@ package patterns;
 
 public class hollow_pattern {
     public static void main(String[] args) {
-        int n=5;
-        for(int i=1; i<=4; i++){
-            for(int j=1; j<=5; j++){
-                if(i==1 || i==4 || j==1 || j==5){
+        int n = 5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i==1 || i==n || j==1 || j==n){
                     System.out.print("*");
                 }
                 else{
--- patterns/inverse_triangle.java
+++ patterns/inverse_triangle.java
@@ -1,14 +1,15 @@
 package patterns;
 
-public class p1 {
+public class inverse_triangle {
     public static void main(String[] args) {
-        // int n = 5;
-        for(int i=0; i<5; i++){
-            for(int j=0; j<5; j++){
+        int n = 5;
+        for(int i=n; i>=0; i--){
+            for(int j=i; j>=0; j--){
                 System.out.print("*");
+
             }
             System.out.println();
-            
+
         }
     }
 }
