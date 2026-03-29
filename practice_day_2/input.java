7d74b877302868ccb426568d72bc7dfb25ad29d6
aiworkspacemohit <ai.workspace.mohit@gmail.com>
2026-03-29 19:49:06 +0530

    addes practice_day_2 folder

---
+++ practice_day_2/input.java
@@ -0,0 +1,13 @@
+package practice_day_2;
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+
+
+    }
+    
+}
---
+++ practice_day_2/variables.java
@@ -0,0 +1,13 @@
+package practice_day_2;
+
+public class variables {
+    public static void main(String[] args) {
+        String name = "Mohit singh";
+        int a = 10;
+        int b = 49;
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+    
+}
