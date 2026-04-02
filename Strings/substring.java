bbce0bc81c994addee8590fe52e086e6af7e2ff2
aiworkspacemohit <ai.workspace.mohit@gmail.com>
2026-04-02 20:44:54 +0530

    string complete

---
+++ Strings/substring.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class substring {
+    public static void main(String args[]){
+        String name = "Rohit kumar";
+        String sub = name.substring(2, name.length());
+        System.out.println(name);
+        System.out.println(sub);
+    }
+}
