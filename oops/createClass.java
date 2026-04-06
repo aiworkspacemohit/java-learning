6f3126250eae2e7b09a06fdd74bd8581402a887e
aiworkspacemohit <ai.workspace.mohit@gmail.com>
2026-04-06 20:36:36 +0530

    added oops folder

---
+++ oops/createClass.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class createClass {
+
+    int age = 30;
+
+    public static void main(String args[]){
+
+        createClass class1 = new createClass();
+        System.out.print(class1.age);
+
+    }
+    
+}
