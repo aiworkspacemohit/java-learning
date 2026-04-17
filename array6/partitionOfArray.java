42779893bcdec7a1e0e5190a632430cc22c5654c
aiworkspacemohit <ai.workspace.mohit@gmail.com>
2026-04-17 22:58:44 +0530

    partitionOfArray

---
+++ array6/partitionOfArray.java
@@ -0,0 +1,17 @@
+package array6;
+import java.util.*;
+
+public class partitionOfArray {
+    //check if we can partition the array into two subarrays with equal sum. 
+    // More fromally , check that the prefix sum of a part of the array is equal to the suffix sum of rest of the array.
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n ;
+        
+
+    }
+
+
+}
